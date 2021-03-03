5eca85cd0bad30f8be9ad301235745633fcf0844
Yury <ymorpheus@gmail.com>
2021-03-03 03:03:00 +0300

    Night themed main page, header elements

--- js/components/header.js
+++ js/components/header.js
@@ -8,11 +8,11 @@ import {
   LOGIN_URL
 } from "../constants.js";
 import currentUser from "../current-user.js";
+import storageService from "../storage-service.js";
+import changeTheme from "../operations/changeTheme.js";
 import template from "../pages-templates/header/header.js";
 import renderSearchPage from "../render/search/search-results.js";
 import { showCabinetWrap, renderCabinetWrap } from "./cabinet-wrap.js";
-import storageService from "../storage-service.js";
-import changeTheme from "../operations/changeTheme.js";
 
 function renderHeader() {
   const header = document.querySelector('header');
@@ -38,22 +38,30 @@ function renderHeader() {
 
   if (currentUser.userData) {
     auth.textContent = 'Кабинет';
-    toggler.setAttribute('src', 'img/nightlight_48dp.svg');
+
+    if (storageService.get('Current theme') === 'Dark') {
+      changeTheme('Dark');
+      storageService.set('Current theme', 'Dark');
+      toggler.setAttribute('src', 'img/daylight_48dp.svg');
+      toggler.setAttribute('title', 'Переключить на светлую тему');
+    }
 
     toggler.addEventListener('click', () => {
       if (storageService.get('Current theme') === 'Light') {
         changeTheme('Dark');
         storageService.set('Current theme', 'Dark');
         toggler.setAttribute('src', 'img/daylight_48dp.svg');
+        toggler.setAttribute('title', 'Переключить на светлую тему');
       } else {
         changeTheme('Light');
         storageService.set('Current theme', 'Light');
         toggler.setAttribute('src', 'img/nightlight_48dp.svg');
+        toggler.setAttribute('title', 'Переключить на темную тему');
       }
-
     });
 
     renderCabinetWrap();
+
     auth.addEventListener('click', showCabinetWrap);
   } else {
     auth.textContent = 'Войти';
