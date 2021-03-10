f3fcb2023abf2f25187263df4f28f0e694db5308
Yury <ymorpheus@gmail.com>
2021-03-10 22:48:54 +0300

    fix in existing quest template

--- js/pages-templates/ready/questionnaire-presentation.js
+++ js/pages-templates/ready/questionnaire-presentation.js
@@ -12,9 +12,9 @@ const template = `
 
     <div id='additional-information-wrap'>
       <h3>Сбор предварительной информации:</h3>
-      <div class='additional-information-anquette card light-theme'>
+      <div class='additional-information-anquette card light-theme'></div>
     </div>
-
+    
     <h4 class='summary'></h4>
     <button type='submit' class='pure-material-button-contained go-to-questions'>Перейти к вопросам</button>
   </form>
